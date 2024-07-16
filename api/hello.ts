f94cc00ae6f75fb22767ee937e58e28e094c3205
Daniel Bond <daniel.bond01@outlook.com>
2024-07-16 08:13:25 +0000

    Log request body

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
+  console.log(req.body)
   return res.json({
-    message: `Hello ${name}!`,
+    success: true,
   })
 }
