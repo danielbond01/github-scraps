d641b4bdbcbc86f9a1edb9e5371d9f65ccc4eeea
Daniel Bond <daniel.bond01@outlook.com>
2024-07-21 16:36:47 +0000

    chore: Add client ID and client secret to config

--- config.ts
+++ config.ts
@@ -6,4 +6,6 @@ export const config = {
   appId: process.env.APP_ID!,
   webhookSecret: process.env.WEBHOOK_SECRET!,
   privateKey: process.env.PRIVATE_KEY!,
+  clientId: process.env.CLIENT_ID!,
+  clientSecret: process.env.CLIENT_SECRET!,
 };
